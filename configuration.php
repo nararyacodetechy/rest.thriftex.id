d48609baac7bb891593867baf3a53fe6d7153e27
Gede Sugandi <gedesugandi@gmail.com>
2023-04-07 10:06:01 +0800

    update config

--- configuration.php
+++ configuration.php
@@ -4,24 +4,24 @@ class SConfig
 {
 
     // online
-    // var $_site_url = 'https://rest.thriftex.id/api/';
-    // var $_app_mode = 'production';
-    // var $_host_name = 'localhost';
-    // var $_database_name = 'thriftex_maindb';
-	// var $_database_user = 'thriftex_maindb';
-	// var $_database_password = 'EEh3qW-%HB_]';
+    var $_site_url = 'https://rest.thriftex.id/api/';
+    var $_app_mode = 'production';
+    var $_host_name = 'localhost';
+    var $_database_name = 'thriftex_maindb';
+	var $_database_user = 'thriftex_maindb';
+	var $_database_password = 'EEh3qW-%HB_]';
     // --- mode
     // *     development
     // *     testing
     // *     production
 
     // offlien
-    var $_site_url = 'http://localhost:8000/my/ci3api/';
-    var $_app_mode = 'development';
-    var $_host_name = 'localhost';
-    var $_database_name = 'thriftex';
-	var $_database_user = 'root';
-	var $_database_password = '';
+    // var $_site_url = 'http://localhost:8000/my/ci3api/';
+    // var $_app_mode = 'development';
+    // var $_host_name = 'localhost';
+    // var $_database_name = 'thriftex';
+	// var $_database_user = 'root';
+	// var $_database_password = '';
 
 	var $_app_version = '1';
 }
